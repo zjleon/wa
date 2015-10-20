e5e904da59a9c4ec9caefe180d6eb443dee2f268
zjleon <zjleon2010@gmail.com>
2015-10-20 19:17:36 +0800

    update jade template

--- testSite/test.js
+++ testSite/test.js
@@ -1,10 +1,14 @@
 var jade = require('jade');
 var fs = require('fs');
+// get the jade template file
 var tmpl = fs.readFileSync('./components/header/header.jade', 'utf8');
+// get the value of variables to pass to the template
 var options = JSON.parse(fs.readFileSync('./components/header/data.json', 'utf8'));
 // console.log(variables);
 
+// parse template to function
 var header = jade.compile(tmpl);
+// pass the values to the template
 header = header(options);
 
 console.log(header);
