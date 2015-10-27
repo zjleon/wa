03e500de10fb599e730054ff7d782c66cdbc9745
zjleon <zjleon2010@gmail.com>
2015-10-27 16:40:30 +0800

    re-init all

--- nodeServer/ITG/pages/test/test.js
+++
@@ -1,7 +0,0 @@
-var myApp = angular.module('myPage', []);
-myApp.controller('myCtrl1', function($scope){
-	$scope.$watch('select1', function(n, o){
-		$scope.name = n;
-	})
-	$scope.select1 = '1';
-});
